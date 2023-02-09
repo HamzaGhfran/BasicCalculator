b0253bbc5918f9206c296f49a7a183fda4da72b0
Hamza <hamzaghafran56@gmail.com>
2023-02-09 17:54:09 +0500

    mpdify calculator features1

--- basicFunction.py
+++ basicFunction.py
@@ -1,10 +1,14 @@
 class Calculator:
-    def __init__(self, num1, num2):
-        self.num1 = num1
-        self.num2 = num2
-        
+    def __init__(self):
+        self.num1 = input("Enter First Number:")
+        self.num2 = input("Enter Second Number:")
+    
     def multiplication(self):
-        print(self.num1 * self.num2)
+        print (int(self.num1) * int(self.num2))
 
-p1 = Calculator(5, 6)
+    def _addition(self):
+        print(int(self.num1) + int(self.num2))
+
+p1 = Calculator()
 p1.multiplication()
+p1._addition()
