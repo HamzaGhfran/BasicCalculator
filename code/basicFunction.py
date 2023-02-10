66020b515bd112330493a0bcd89d02de3a028813
Hamza <hamzaghafran56@gmail.com>
2023-02-10 22:29:02 +0500

    resolve comment

--- code/basicFunction.py
+++ code/basicFunction.py
@@ -1,17 +1,20 @@
 class Calculator:
-    def __init__(self):
-        self.num1 = input("Enter First Number:")
-        self.num2 = input("Enter Second Number:")
     
-    def multiplication(self):
-        print (int(self.num1) * int(self.num2))
+        """
+        This class is used to perform basic arthmathic operations (+, -, *, /)
+        over two integers.
+        In this  class input get by user.
+        """
+        def __init__(self):
+            self.num1 = input("Enter First Number:")
+            self.num2 = input("Enter Second Number:")
+        
+        def multiplication(self):
+            print (int(self.num1) * int(self.num2))
 
-    def _addition(self):
-        print(int(self.num1) + int(self.num2))
-    
-    def _subtraction(self):
-        print(int(self.num1) - int(self.num2))
+        def addition(self):
+            print(int(self.num1) + int(self.num2))
+        
+        def subtraction(self):
+            print(int(self.num1) - int(self.num2))
 
-p1 = Calculator()
-p1.multiplication()
-p1._addition()
